3b45971898609028eeec836c8ca0303f98465ce7
Matthew Phelps <matthew.w.phelps18@gmail.com>
2022-07-06 14:46:01 -0500

    Added getData and displayUsers functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+    const usersRequest = await fetch (
+        "https://randomuser.me/api?results=5"
+    )
+    const data = await usersRequest.json();
+    //console.log(data);
+    const userResults = data.results;
+    //console.log(userResults);
+    displayUsers(userResults)
+};
+
+const displayUsers = function(userResults) {
+    randomFolks.innerHTML = "";
+    for (let user of userResults) {
+        let country = user.location.country;
+        let name = `${user.name.first} ${user.name.last}`;
+        let imageUrl = user.picture.medium;
+        let userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+        <h3>${name}</h3>
+        <p>${country}</p>
+        <img src=${imageUrl} alt=User avatar />
+        `;
+        randomFolks.append(userDiv);
+    }
+};
+getData();
